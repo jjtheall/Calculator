121e464f08e0bf22267eb044751b88b529b97067
jjtheall <jtheall@quinnipiac.edu>
2022-02-07 17:33:45 -0500

    allows user to enter two numbers and prints sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String[] args){}
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+
+    }
 }
