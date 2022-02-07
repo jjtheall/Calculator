312995ad63cd33ade7c8504b5eb154c4d72800bd
jjtheall <jtheall@quinnipiac.edu>
2022-02-07 17:44:51 -0500

    allows user to select whether they would like to add or subtract

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -9,7 +9,14 @@ public class Calculator {
         int num1 = sc.nextInt();
         System.out.println("Enter the second number: ");
         int num2 = sc.nextInt();
-        System.out.println(num1 + num2);
-
+        sc.nextLine();
+        System.out.println("Add or subtract? (Enter \"+\" or \"-\")");
+        String operation = sc.nextLine();
+        if(operation.equals("+")){
+            System.out.println(num1 + num2);
+        }
+        else{
+            System.out.println(num1 - num2);
+        }
     }
 }
